dfb0f24275ead64e1e568ae9776d5eb2cfdb029c
asmialek <smialekadam@gmail.com>
2019-08-30 13:44:29 +0200

    Semi final version of stabilization simulation

---
+++ pid_tests/bexus_simple_pid_setup.m
@@ -0,0 +1,15 @@
+Kp = 1;
+Ki = 0.1;
+Kd = 0.1;
+
+tau = 1.67644024;
+
+gyro_noise = ((deg2rad(0.15))^2)*3600;
+gyro_freq = 100;
+
+motor_speed = 0.2272729091;
+
+h_amp_noise_power = 0.0001;
+l_amp_noise_power = 0.0000;
+
+random_seed = 23351;
